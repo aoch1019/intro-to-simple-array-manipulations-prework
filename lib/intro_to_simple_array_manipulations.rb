3aee584b8f46920aa3526496626e347e43b799cd
Andrew Och <andrewoch95@gmail.com>
2018-07-06 03:37:36 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,8 @@
-def using_push(array,string)
+def using_push(array, string)
   array.push(string)
 end
 
-def using_unshift(array,string)
+def using_unshift(array, string)
   array.unshift(string)
 end
 
@@ -28,4 +28,8 @@ def shift_with_args(array, number = 2)
     new_array.push(array.shift)
   end
   return new_array
+end
+
+def using_concat(array1, array2)
+  
 end
